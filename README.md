f1127901e64b185aba7ac08d6f0fb1633c6987d9
Shigeki Shoji <takesection@gmail.com>
2025-08-06 19:28:28 +0900

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+
+bucketName = ssyouzihnd
+key = badges/reactive-architecture-domain-driven-design-level-2.png
---
+++ src/main/java/com/pigumer/BadgeHandler.java
@@ -0,0 +1,38 @@
+package com.pigumer;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.pigumer.logic.Logic;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class BadgeHandler implements RequestHandler<BadgeHandler.BadgeObject, String> {
+
+    public record BadgeObject(String bucketName, String key) {}
+
+    private static final S3Client s3Client = S3Client.builder().region(Region.AP_NORTHEAST_1).build();
+
+    @Override
+    public String handleRequest(BadgeObject in, Context context) {
+        context.getLogger().log(in.toString());
+        GetObjectRequest req = GetObjectRequest.builder()
+                .bucket(in.bucketName)
+                .key(in.key)
+                .build();
+        ResponseInputStream<GetObjectResponse> res = s3Client.getObject(req);
+        try (ByteArrayInputStream stream = new ByteArrayInputStream(res.readAllBytes())) {
+            Logic logic = new Logic();
+            return logic.analyze(stream);
+        } catch (Exception e) {
+            context.getLogger().log(e.getMessage());
+            return e.getMessage();
+        }
+    }
+}
