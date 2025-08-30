ffd34ccb190f0f4c5c53d1d5e4ebb04c89bc40d6
Shigeki Shoji <takesection@gmail.com>
2025-08-30 19:38:38 +0900

    update

---
+++ src/main/java/com/pigumer/database/OpenBadgesTable.java
@@ -0,0 +1,42 @@
+package com.pigumer.database;
+
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.PutItemResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OpenBadgesTable {
+
+    private DynamoDbClient client;
+
+    private String tableName;
+
+    public OpenBadgesTable(String tableName) {
+        client = DynamoDbClient.builder().build();
+        this.tableName = tableName;
+    }
+
+    public PutItemResponse put(Map<String, Object> map) {
+        String badge = (String) map.get("badge");
+        String id = (String) map.get("id");
+        String identity = (String) ((Map<String, String>) map.get("recipient")).get("identity");
+        String issuedOn =  (String) map.get("issuedOn");
+        String row = map.toString();
+        HashMap<String, AttributeValue> itemMap = new HashMap<>();
+        itemMap.put("badge", AttributeValue.builder().s(badge).build());
+        itemMap.put("id", AttributeValue.builder().s(id).build());
+        itemMap.put("issuedOn", AttributeValue.builder().s(issuedOn).build());
+        itemMap.put("identity", AttributeValue.builder().s(identity).build());
+        itemMap.put("row", AttributeValue.builder().s(row).build());
+        PutItemRequest putItemRequest = PutItemRequest.builder()
+                .tableName(tableName)
+                .item(itemMap)
+                .build();
+        PutItemResponse res = client.putItem(putItemRequest);
+        System.out.println(res.toString());
+        return res;
+    }
+}
